7538e72378e0624911a0e5c7c74a1174b5d18bae
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-10-27 21:51:20 +0300

    Добавил подхват timestamp

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import os
+import logging
 
 import requests
 from dotenv import load_dotenv
 
 
-def main():
-    load_dotenv()
-    devman_token = os.environ['DEVMAN_TOKEN']
+def devman_request(url, devman_token):
     headers = {
         'Authorization': devman_token
     }
-    url = 'https://dvmn.org/api/long_polling/'
+    params = {}
     while True:
         try:
-            response = requests.get(url, headers=headers, timeout=5)
+            response = requests.get(url, headers=headers, params=params)
+            json_response = response.json()
+            if json_response['status'] == 'timeout':
+                timestamp = json_response['timestamp_to_request']
+            else:
+                timestamp = json_response['last_attempt_timestamp']
+            params = {
+                'timestamp': str(timestamp)
+            }
             print(response.json())
         except requests.exceptions.ReadTimeout:
             continue
-        except ConnectionError:
+        except requests.exceptions.ConnectionError:
             continue
+
+
+def main():
+    load_dotenv()
+    logging.basicConfig(filename="sample.log", level=logging.INFO)
+    devman_token = os.environ['DEVMAN_TOKEN']
+    url = 'https://dvmn.org/api/long_polling/'
+    devman_request(url, devman_token)
+
 
 if __name__ == '__main__':
     main()
