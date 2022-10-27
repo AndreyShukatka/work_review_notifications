70ae0c4bc822db91acb5246cd07ce0c48e8574ed
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-10-27 13:23:18 +0300

    Воспользовался Long Polling API

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
     devman_token = os.environ['DEVMAN_TOKEN']
-    headers = {'Authorization': devman_token}
-    url = 'https://dvmn.org/api/user_reviews/'
+    headers = {
+        'Authorization': devman_token
+    }
+    url = 'https://dvmn.org/api/long_polling/'
     response = requests.get(url, headers=headers)
     print(response.text)
 
