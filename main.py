ad9f0fb249c7da61b7db4530abab791792617486
AndreyShukatka <9080106@gmail.com>
2022-10-30 23:07:37 +0300

    Написал обработчик логов

--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import os
 from time import sleep
 import textwrap
 import logging
+from logging.handlers import RotatingFileHandler
 
 import telegram
 import requests
 from dotenv import load_dotenv
 
+class TelegramLogsHandler(logging.Handler):
 
-def send_message_bot(telegram_token, tgm_id, json_response):
+    def __init__(self, telegram_token, tgm_id):
+        super().__init__()
+        tg_bot = telegram.Bot(token=telegram_token)
+        self.chat_id = tgm_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def send_message_bot(telegram_token, tgm_id, review_answer):
     bot = telegram.Bot(token=telegram_token)
-    lesson_title = json_response['new_attempts'][0]['lesson_title']
-    lesson_url = json_response['new_attempts'][0]['lesson_url']
-    if json_response['new_attempts'][0]['is_negative']:
+    lesson_title = review_answer['new_attempts'][0]['lesson_title']
+    lesson_url = review_answer['new_attempts'][0]['lesson_url']
+    if review_answer['new_attempts'][0]['is_negative']:
         bot.send_message(
             text=textwrap.dedent(
                 f'''У вас проверили работу "{lesson_title}"
@@ -45,9 +58,12 @@ def main():
     }
     params = {}
     seconds = 20
+    logger = logging.getLogger('Logger')
+    logger.setLevel(logging.INFO)
+    logger.addHandler(TelegramLogsHandler(telegram_token, tgm_id))
+    logger.info('Бот запущен!')
     while True:
         try:
-            logging.warning('Бот запущен!')
             response = requests.get(url, headers=headers, params=params)
             response.raise_for_status()
             review_answer = response.json()
@@ -61,11 +77,17 @@ def main():
             }
 
         except requests.exceptions.ReadTimeout:
+            logger.exception('Время ожидания ответа превышено!')
             continue
         except requests.exceptions.ConnectionError:
+            logger.exception('Нет соединения с сервером!')
             sleep(seconds)
+            continue
+        except Exception:
+            logger.exception("Бот Упал")
             continue
 
 
 if __name__ == '__main__':
     main()
+
